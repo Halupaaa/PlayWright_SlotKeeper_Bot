94c5ee40a525f81fe578ed5955da54015b4856f0
Halupaaa <h.bondarchuk01@gmail.com>
2026-03-31 19:02:31 +0300

    fixing bag

--- PlayWright_SlotKeeper_Bot/Program.cs
+++ PlayWright_SlotKeeper_Bot/Program.cs
@@ -8,20 +8,26 @@ class Program
 {
     static async Task Main()
     {
+
+        string botToken = "8710843756:AAGBdfHxcFrfLz2a32olOAJQgBD50OG0o6Q";
+        string chatId = "631232411";
+
+        using (var httpClient = new HttpClient())
+        {
+            await httpClient.GetAsync($"https://api.telegram.org/bot{botToken}/sendMessage?chat_id={chatId}&text={Uri.EscapeDataString("Bot has access to tg")}");
+        }
+
         List<string> urls = new List<string>
         {
             "https://calendly.com/tesliuk-volodymyr-lll/10",
             "https://calendly.com/kasyk3/labs"
         };
 
-        string botToken = "8710843756:AAGBdfHxcFrfLz2a32olOAJQgBD50OG0o6Q";
-        string chatId = "631232411";
-
         using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
         var page = await browser.NewPageAsync();
 
-        Console.WriteLine("Bot monitors");
+        Console.WriteLine("Bot monitors. It will send you a message when any slot will be availible.");
 
         while (true)
         {
@@ -30,6 +36,8 @@ class Program
                 try
                 {
                     await page.GotoAsync(url);
+                    await page.GotoAsync(url, new PageGotoOptions { WaitUntil = WaitUntilState.NetworkIdle });
+
                     await page.WaitForTimeoutAsync(5000);
 
                     var availableDays = await page.Locator("button[data-component='calendar-day']:not([disabled])").CountAsync();
